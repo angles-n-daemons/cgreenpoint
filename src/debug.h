4782b5d960ecc784888945693475b4c5ceb9492e
Brian Dillman <bdillman@squareup.com>
2022-04-20 22:15:17 -0400

    add disassembler

---
+++ src/debug.h
@@ -0,0 +1,9 @@
+#ifndef cgreenpoint_debug_h
+#define cgreenpoint_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
 	Chunk chunk;
 	initChunk(&chunk);
 	writeChunk(&chunk, OP_RETURN);
+	disassembleChunk(&chunk, "hello");
 	freeChunk(&chunk);
 
-	printf("hello brooklyn");
 	return 0;
 }
