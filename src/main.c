724e32de2977be1ca9ed6412b5c090196b54d3d7
Brian Dillman <bdillman@squareup.com>
2022-04-27 18:25:40 -0400

    added lines to implementation

--- src/main.c
+++ src/main.c
@@ -6,8 +6,11 @@ int main(int argc, const char* argv[]) {
 	Chunk chunk;
 	initChunk(&chunk);
 	int constant = addConstant(&chunk, 1.2);
-	writeChunk(&chunk, OP_CONSTANT);
-	writeChunk(&chunk, constant);
+
+	writeChunk(&chunk, OP_CONSTANT, 1);
+	writeChunk(&chunk, constant, 1);
+	writeChunk(&chunk, OP_RETURN, 3);
+
 	disassembleChunk(&chunk, "hello");
 	freeChunk(&chunk);
 
