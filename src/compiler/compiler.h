e929f8330f8e0a754db9f702845dda6fb6457a9a
Brian Dillman <bdillman@squareup.com>
2022-05-20 19:52:36 -0400

    finish out what i believe is the beginning of the compiler code

--- src/compiler/compiler.h
+++ src/compiler/compiler.h
@@ -1,6 +1,8 @@
 #ifndef cgreenpoint_compiler_h
 #define cgreenpoint_compiler_h
 
-void compile(const char* source);
+#include "../vm/chunk.h"
+
+bool compile(const char* source, Chunk* chunk);
 
 #endif
