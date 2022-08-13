53875304175f90b52844cf895886f18f2f474577
Brian Dillman <bdillman@squareup.com>
2022-08-13 14:02:49 -0400

    only allow for real assignment targets

--- src/test/object/test_variables.c
+++ src/test/object/test_variables.c
@@ -7,8 +7,13 @@ void testSimpleVariables() {
     runTest("var a = 5; a = 4; print a;", "4");
 }
 
+void testInvalidAssignment() {
+    runTest("var a = 4; b = 3; a * b = 5;", "Invalid assignment target.");
+}
+
 void testVariables() {
     printf("testTable starting\n");
     testSimpleVariables();
+    testInvalidAssignment();
     printf("testTable completed\n");
 }
