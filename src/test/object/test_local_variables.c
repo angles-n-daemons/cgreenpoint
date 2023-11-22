273ef56c94203cc1f0bd57546ee7bfc9d4f48184
Brian Dillmann <dillmann.brian@gmail.com>
2023-11-22 17:24:54 -0500

    add one more test for local variables

--- src/test/object/test_local_variables.c
+++ src/test/object/test_local_variables.c
@@ -7,8 +7,20 @@ void testLocalVariablesBasic() {
     runTest("{ var a = 5; { var b = 2; print a + b; } }", "7");
 }
 
+void testLocalVariablesMultiple() {
+    runTest("{ var a = 5; var b = 2; var c = 1; print b; }", "2");
+}
+
+
+void testMultipleLevelsLocalVariables() {
+    runTest("var a = 5; { var a = 3; {var a = 1; print a; }}", "1");
+}
+
+
 void testLocalVariables() {
     printf("testLocalVariables starting\n");
     testLocalVariablesBasic();
+    testLocalVariablesMultiple();
+    testMultipleLevelsLocalVariables();
     printf("testLocalVariables completed\n");
 }
