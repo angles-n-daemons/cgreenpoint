3099ba7d865a92bd903d12f1df098940297aa22f
Brian Dillmann <dillmann.brian@gmail.com>
2024-02-07 19:51:58 -0500

    add main test to function

--- src/test/function/test_function.c
+++ src/test/function/test_function.c
@@ -3,7 +3,27 @@
 
 #include "../test_common.h"
 
+void testFunctionBasic() {
+    runTest("\
+    fun first() {\
+        var a = 1;\
+        second();\
+        var b = 2;\
+        print a + b;\
+        second();\
+    }\
+    \
+    fun second() {\
+        var c = 3;\
+        var d = 4;\
+        print c + d;\
+    }\
+    \
+    first()", "\n7\n3\n7");
+}
+
 void testFunction() {
     printf("testFunction starting\n");
+    testFunctionBasic();
     printf("testFunction completed\n");
 }
