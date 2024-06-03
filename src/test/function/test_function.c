0c6202757ba944891d5d2c9f8d19eb5f23a565c0
angles-n-daemons <dillmann.brian@gmail.com>
2024-06-03 13:26:45 -0400

    finish off defining functions

--- src/test/function/test_function.c
+++ src/test/function/test_function.c
@@ -3,29 +3,39 @@
 
 #include "../test_common.h"
 
-void testFunctionBasic() {
+// void testFunctionBasic() {
+//   runTest("\
+//    fun first() {\
+//        var a = 1;\
+//        second();\
+//        print a;\
+//        var b = 2;\
+//        second();\
+//        print a + b;\
+//    }\
+//    \
+//    fun second() {\
+//        var c = 3;\
+//        var d = 4;\
+//        print c + d;\
+//    }\
+//    \
+//    first()",
+//           "\n7\n1\n3\n7");
+// }
+
+void testDeclareFunction() {
   runTest("\
-    fun first() {\
-        var a = 1;\
-        second();\
-        print a;\
-        var b = 2;\
-        second();\
-        print a + b;\
-    }\
-    \
-    fun second() {\
-        var c = 3;\
-        var d = 4;\
-        print c + d;\
-    }\
-    \
-    first()",
-          "\n7\n1\n3\n7");
+    fun areWeHavingItYet() { \
+        print \"Yes we are!\";\
+    } \
+    print areWeHavingItYet;",
+          "<fn areWeHavingItYet>");
 }
 
 void testFunction() {
   printf("testFunction starting\n");
-  testFunctionBasic();
+  // testFunctionBasic();
+  testDeclareFunction();
   printf("testFunction completed\n");
 }
