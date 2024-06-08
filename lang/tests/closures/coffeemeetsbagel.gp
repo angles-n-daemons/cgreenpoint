e6eb2da6e4a43af5fcf42aed2be35657126dd976
Brian Dillmann <dillmann.brian@gmail.com>
2024-06-08 16:55:26 -0400

    make greenpoint files debuggable in nvim-dap

---
+++ lang/tests/closures/coffeemeetsbagel.gp
@@ -0,0 +1,11 @@
+fun makeClosure(value) {
+	fun closure() {
+		print closure;
+	}
+	return closure;
+}
+
+var doughnut = makeClosure("doughnut");
+var bagel = makeClosure("doughnut");
+doughnut();
+bagel();
--- src/test/function/test_closure.c
+++ src/test/function/test_closure.c
@@ -16,8 +16,24 @@ void testClosureBasic() {
           "outer");
 }
 
+void testCoffeeMeetsBagel() {
+  runTest("\
+    fun makeClosure(value) { \
+      fun closure() { \
+        print value; \
+      } \
+      return closure; \
+    } \
+    var doughnut = makeClosure(\"doughnut\"); \
+    var bagel = makeClosure(\"bagel\"); \
+    doughnut(); \
+    bagel();",
+          "doughnut\nbagel");
+}
+
 void testClosure() {
   printf("testClosure starting\n");
   testClosureBasic();
+  testCoffeeMeetsBagel();
   printf("testClosure completed\n");
 }
